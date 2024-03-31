e7d64f3490b8915ae883e3e30765309f66a92094
yanderbikovmail.ru <yanisderbikov@gmail.com>
2024-03-31 15:10:40 +0800

    deploy 2

--- src/main/java/com/zhurzh/linksredirect/excetions/GlobalExceptionHandler.java
+++ src/main/java/com/zhurzh/linksredirect/excetions/GlobalExceptionHandler.java
@@ -1,33 +1,15 @@
 package com.zhurzh.linksredirect.excetions;
 
-import lombok.extern.log4j.Log4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.Arrays;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
-@Log4j
 public class GlobalExceptionHandler {
-    @Value("${domain.redirect}")
-    private String urlNotFound;
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        var list = Arrays.stream(e.getStackTrace()).toList();
-        log.error(e.getMessage());
-        log.error(list);
-        return new ResponseEntity<>(
-                "An error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler(Exception.class)
     public RedirectView handleNoSuchElementException(Exception e) {
-        log.info(e.getMessage());
-        return new RedirectView(urlNotFound);
+        return new RedirectView("https://www.furaffinity.net/user/zhurzh/");
     }
 }
